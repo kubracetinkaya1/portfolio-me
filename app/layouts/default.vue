fa967998aa6a4a4e581da5306482492db84d5375
kubracetinkaya1 <kubra.ceitnkaya0@ogr.dpu.edu.tr>
2025-09-01 11:32:07 +0300

    scrol iphone için ayarlandı

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
-import { useRoute } from 'vue-router'
-
 const route = useRoute()
 </script>
 
 <template>
-  <div class="absolute inset-0 w-screen h-screen flex flex-col">
+  <div class="absolute inset-0 w-screen min-h-[100dvh] flex flex-col">
     <div
-      class="absolute inset-0 flex flex-col w-full h-full z-9999 scroll-smooth"
+      class="absolute inset-0 flex flex-col w-full h-full z-9999 scroll-smooth scroll-smooth-ios"
       :class="[
         route.name === 'home'
           ? 'overflow-y-auto md:overflow-y-hidden scrollbar-thin'
